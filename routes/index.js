e052dca7ed28a2fc807edad613dd279dad592c8b
Ben Gögge <bn@ggge.de>
2015-11-15 19:01:13 +0100

    changes to google login

--- routes/index.js
+++ routes/index.js
@@ -39,11 +39,12 @@ router.get('/logout', function(req, res, next) {
 });
 
 router.get('/auth/google',
-  passport.authenticate('google', { scope: 'https://www.googleapis.com/auth/plus.login' }));
+  passport.authenticate('google', { scope: 'https://www.googleapis.com/oauth2/v2/userinfo.email' }));
 
 router.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
+      console.log(req);
     // Successful authentication, redirect home.
     res.redirect('/');
   });
