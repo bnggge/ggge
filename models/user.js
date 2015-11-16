223583fe333ef2bd717f5424fa0d218018189dd0
Benjamin Goegge <bn@ggge.de>
2015-11-16 12:47:04 +0100

    model-changes

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ var User = new Schema({
     googleId: String
 });
 
-User.plugin(passportLocalMongoose, {usernameField: 'email', usernameQueryFields: ['username', 'twitterId', 'facebookId', 'googleId'], populateFields: ['username', 'photo', 'name', 'twitterId', 'facebookId', 'googleId']});
+User.plugin(passportLocalMongoose, {usernameField: 'email', usernameQueryFields: ['username', 'twitterId', 'facebookId', 'googleId']});
 User.plugin(findOrCreate);
 
 module.exports = mongoose.model('User', User);
