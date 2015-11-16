1ec111cd1e84caeaeffe3ee2a98d71bc2f908451
Ben Gögge <bn@ggge.de>
2015-11-16 17:20:19 +0100

    use mongoose for update

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ passport.use(new GoogleStrategy({
      });
     } else {
       console.log(req.user.username+ "is logged in and adds "+ profile.emails[0].value +" from "+profile.provider);
-      User.findOrCreate( { _id: mongoose.Types.ObjectId(req.user._id) }, { googleId: profile.id }, { options: { upsert: true } } , function (err, user) {
+      User.findByIdAndUpdate( req.user._id, { googleId: profile.id } , function (err, user) {
         return done(err, user);
       });
     }
