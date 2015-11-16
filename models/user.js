948159327b747a49f8e4dd57a607c494f6e911e0
Ben Gögge <bn@ggge.de>
2015-11-16 15:05:54 +0100

    unique entities in model and creationdate

--- models/user.js
+++ models/user.js
@@ -4,18 +4,19 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var findOrCreate = require('mongoose-findorcreate');
 
 var User = new Schema({
-    username: String,
-    name: String,
+    username: { type: String, unique: true },
+    fullname: String,
     photo: String,
     password: String,
     phone: String,
-    email: String,
-    twitterId: String,
-    facebookId: String,
-    googleId: String
+    email: { type: String, unique: true },
+    twitterId: { type: String, unique: true },
+    facebookId: { type: String, unique: true },
+    googleId: { type: String, unique: true },
+    createdAt: { type: Date, default: Date.now }
 });
 
-User.plugin(passportLocalMongoose, {usernameField: 'username', usernameQueryFields: ['email', 'twitterId', 'facebookId', 'googleId']});
+User.plugin(passportLocalMongoose, {usernameField: 'username', usernameQueryFields: ['email', 'twitterId', 'facebookId', 'googleId'], usernameLowerCase: true});
 User.plugin(findOrCreate);
 
 module.exports = mongoose.model('User', User);
