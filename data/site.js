15a1d3ab5e9060819120652fa04cb8676e694603
Ben Gögge <bn@ggge.de>
2015-11-27 16:14:57 +0100

    Internationalization

---
+++ data/site.js
@@ -0,0 +1,72 @@
+var site = {
+    en: {
+      url: 'https://ggge.eu'
+    },
+    de: {
+      url: 'https://ggge.de'
+    },
+    index: {
+      type: 'Website',
+      photos: [''],
+      en: {
+        slug: '',
+        name: 'ggge.eu',
+        title: 'Startup services by Benjamin Goegge',
+        url: 'https://ggge.eu',
+        description: 'Benjamin Goegge supports startups in Berlin building and optimizing their digital products.',
+        content: '',
+        tags: [''],
+        keywords: [''],
+        categories: ['']
+      }, 
+      de: {
+        slug: '',
+        name: 'ggge.de',
+        title: 'Dienstleistungen für Startups in Berlin',
+        url: 'https://ggge.eu',
+        description: 'Benjamin Gögge unterstützt Startups in Berlin bei der Konzeption, Umsetzung und Optimierung ihrer digitalen Produkte.',
+        content: '',
+        tags: [''],
+        keywords: [''],
+        categories: ['']
+      }
+    },
+    imprint: {
+      type: 'website',
+      de: {
+        slug: 'impressum',
+        name: 'Impressum',
+        url: 'https://ggge.de/impressum',
+        title: 'Impressum',
+        description: 'ggge.de beschreibt Benjamin Gögges Dienstleistungen für Startups in Berlin',
+        content: '',
+        tags: [''],
+        keywords: [''],
+        categories: ['']
+      },
+      en: {
+        slug: 'imprint',
+        name: 'Imprint',
+        url: 'https://ggge.eu/imprint',
+        title: 'Imprint',
+        description: 'ggge.eu describes the services of Benjamin Goegge for startups in Berlin.',
+        content: '',
+        tags: [''],
+        keywords: [''],
+        categories: ['']
+      } 
+    }, 
+    toc: {
+      slug: 'agb',
+      name: 'AGB'
+    }, 
+    contact: {
+      slug: 'kontakt',
+      name: 'Kontakt'
+    },
+    about: {
+      slug: 'about',
+      name: 'Über mich'
+    }
+  }
+module.exports = site;
--- models/page.js
+++ models/page.js
@@ -2,18 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Page = new Schema({
-    title: { type: String, trim: true },
-    description: { type: String, trim: true },
-    summary: { type: String, trim: true },
-    content: { type: String, trim: true },
-    tags: [{ type: String, trim: true, lowercase: true }],
-    categories: [{ type: String, trim: true, lowercase: true }],
-    keywords: [{ type: String, trim: true, lowercase: true }],
-    slug: { type: String, trim: true, lowercase: true },
-    name: { type: String, trim: true },
-    url: { type: String, trim: true, lowercase: true },
-    language: { type: String, default: 'en' },
-    translations: [{
+    en: {
         title: { type: String, trim: true },
         description: { type: String, trim: true },
         summary: { type: String, trim: true },
@@ -21,22 +10,34 @@ var Page = new Schema({
         tags: [{ type: String, trim: true, lowercase: true }],
         categories: [{ type: String, trim: true, lowercase: true }],
         keywords: [{ type: String, trim: true, lowercase: true }],
-        slug: { type: String, trim: true, lowercase: true },
+        slug: { type: String, trim: true, lowercase: true, unique: true },
         name: { type: String, trim: true },
-        url: { type: String, trim: true, lowercase: true },
-        language: { type: String, default: 'de' }
-    }],
+        url: { type: String, trim: true, lowercase: true, unique: true }
+    },
+    de: {
+        title: { type: String, trim: true },
+        description: { type: String, trim: true },
+        summary: { type: String, trim: true },
+        content: { type: String, trim: true },
+        tags: [{ type: String, trim: true, lowercase: true }],
+        categories: [{ type: String, trim: true, lowercase: true }],
+        keywords: [{ type: String, trim: true, lowercase: true }],
+        slug: { type: String, trim: true, lowercase: true, unique: true },
+        name: { type: String, trim: true },
+        url: { type: String, trim: true, lowercase: true, unique: true }
+    },
     images: [{ type: String, trim: true, lowercase: true }],
     attachments: [{ type: String, trim: true, lowercase: true }],
     thumbnail: { type: String, trim: true, lowercase: true },
     author: { type: mongoose.Schema.Types.ObjectId },
     status: { type: String, trim: true, lowercase: true, default: 'draft' },
+    type: { type: String, default: 'article' },
     date: { type: Date, default: Date.now },
     modified: { type: Date, default: Date.now },
     nav: { type: Boolean, default: false },
     footer: { type: Boolean, default: false },
-    template: { type: String, trim: true, lowercase: true, default: 'main' },
-    parent: { type: String, trim: true, lowercase: true, default: 'main' }
+    template: { type: String, trim: true, lowercase: true, default: 'index' },
+    parent: { type: String, trim: true, lowercase: true, default: 'index' }
 });
 
 
